19ec281c9b779d7fdd48a7353093fff1425c3351
User <User@LAB3-5>
2021-09-13 14:32:59 +0300

    Add project files.

---
+++ LicenseServer/Controllers/LicenseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LicenseServerBL.Models;
+
+namespace LicenseServer.Controllers
+{
+    [Route("LicenseAPI")]
+    [ApiController]
+    public class LicenseController : ControllerBase
+    {
+        LicenseDBContext context;
+        public LicenseController(LicenseDBContext context)
+        {
+            this.context = context;
+        }
+    }
+}
